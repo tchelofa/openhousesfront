da69feb9d7dbbe7ca0daa076e9df40353d7e1897
Marcelo Silva <marceloadss@gmail.com>
2024-08-05 15:20:58 +0100

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,12 @@
 'use client'
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
 import { useState, useEffect } from "react";
 import CookieConsent, { getCookieConsentValue } from "react-cookie-consent";
+import { metadata } from "./metadata";  // Importar metadata
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "OpenHouses - Your home with respect.",
-  description: "Tired of wasting time on Irish property listings? OpenHouses connects respectful tenants with qualified landlords. Find your perfect match now! Browse thousands of listings, connect directly with landlords, and rent with confidence. Visit our website and start your search today!",
-};
 
 export default function RootLayout({
   children,
---
+++ src/app/metadata.ts
@@ -0,0 +1,7 @@
+// src/app/metadata.ts
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "OpenHouses - Your home with respect.",
+  description: "Tired of wasting time on Irish property listings? OpenHouses connects respectful tenants with qualified landlords. Find your perfect match now! Browse thousands of listings, connect directly with landlords, and rent with confidence. Visit our website and start your search today!",
+};

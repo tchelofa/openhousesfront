064192f5710be36c4670e2388f25b8aa5bf731ee
Marcelo Silva <marceloadss@gmail.com>
2024-08-05 15:02:33 +0100

    google ads, google analitics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,13 +17,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager */}
+        <Script async src={`https://www.googletagmanager.com/gtag/js?id=G-ZNJTH4YYRC`} strategy="afterInteractive" />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-ZNJTH4YYRC');
+          `}
+        </Script>
+        {/* Google Ads */}
+        <Script 
+          async 
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7617273455696170"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
       <body className={inter.className}>
-        
         {children}
       </body>
     </html>
   );
 }
-
-
-
